cea9f4358a1c4856158ecee0bbfb5c142f77af34
Lukas Buchli <lukas.buchli@ost.ch>
2026-03-09 19:57:21 +0100

    Configure devenv

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-18.11";
+      url = "github:nixos/nixpkgs/nixos-25.11";
     };
     flake-utils = {
       url = "github:numtide/flake-utils";
@@ -12,17 +12,44 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-    in rec {
+      pythonEnv = pkgs.python3.withPackages(ps: with ps; [
+        ipython
+        jupyter
+        numpy
+        scipy
+        matplotlib
+        (buildPythonPackage rec {
+          pname = "jupyterlab_rise";
+          version = "0.43.1";
+          format = "wheel";
+          src = pkgs.fetchurl {
+            url = "https://files.pythonhosted.org/packages/5d/6b/ccc3f8d9940719b1d6c0c12aab9417d4008ff6aa419c8d103aa4c29ee3d1/jupyterlab_rise-0.43.1-py3-none-any.whl";
+            hash = "sha256-YrptjAeoDf8pNYlRLVeQECmACiOoYJBLFOuW1ZfLhXM=";
+          };
+        })
+      ]);
+    in {
+
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "presentation";
+        version = "1.0.0";
+        src = ./.;
+
+        buildInputs = [ pythonEnv ];
+
+        buildPhase = ''
+          export HOME=$(mktemp -d)
+          jupyter nbconvert presentation.ipynb --to slides
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp presentation.slides.html $out/index.html
+        '';
+      };
+
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (python3.withPackages(ps: with ps; [
-            ipython
-            jupyter
-            numpy
-            scipy
-            pillow
-          ]))
-        ];
+        buildInputs = [ pythonEnv ];
         shellHook = "echo \"start with 'jupyter notebook'\"";
       };
     }
